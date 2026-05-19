16207e92647a39decb9600288e56e3496e40f6db
borislinn <262165815+borislinn@users.noreply.github.com>
2026-05-19 15:07:56 +0800

    Use grid layout for transfer route chips

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "jp-trip-v44";
+const CACHE = "jp-trip-v45";
 const SHELL = [
   "./", "./index.html", "./styles.css", "./app.webmanifest",
   "./icons/icon-192.png", "./icons/icon-512.png",
